2ef0f1d11ddb124dee176199739fb20e61781166
juntae <jkim3389@gatech.edu>
2020-04-24 23:28:23 -0400

    Restructured class

--- README.md
+++ README.md
@@ -2,3 +2,35 @@
 
 Learning TypeScript by making a simple Blockchain with it
 
+## Blockchain
+
+Blockchains are built through a combination of linked list and merkle tree. 
+A blockchain is a chain of block linked to one another using linked list structure. 
+Instead of traditional pointer that point to next node, blockchain use "previous block's hash" to refer previous block. In this project, we are not implementing merkle tree for simplicity. 
+
+Below how the blockchian is structured. 
+
+![Linked List and Merkle Tree](/Assets/blockchain_structure.png)
+
+### Block Object - Input
+
+ each object takes timestamp and data. 
+
+Timestamp shows when the block was created. It is useful because the system can use this timestamp to rebalance the mininng difficulty every month. 
+
+The other input, Data, holds information stored in the chain. In most of cryptocurrencies, this is where the transaction data is stored in form of merkle tree. 
+
+### Block Object - Data
+
+In each block object, there are 4 more datas, index, previousHash, hash, and nonce. 
+
+Index  - communicate where in the chain the block is located. Remember that "blockchain is implemented as linked list and this index let us know the position of each block in blockchain"
+
+PreviousHash - indicates the hashvalue of previous block in blockchain. Instead of pointer to point next block or previous block in traditional linked list, block chain use previousHash value. 
+
+Hash - hold the result from calculateHash(). 
+
+nonce - it is critical to building in a mining mechanism. 
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ class Block {
     public previousHash : string;
     public data : string;
     public timestamp : number;
+    public nonce : number;
 
     static calculateBlockHash = (index:number, previousHash:string, timestamp: number, data:string):string  => CrpytoJS.SHA256(index+previousHash+timestamp+data).toString();
     static checkValidBlockStructure = (block : Block) : boolean => {
@@ -14,7 +15,8 @@ class Block {
             typeof block.hash ==='string' && 
             typeof block.previousHash ==='string' && 
             typeof block.data ==='string' && 
-            typeof block.timestamp ==='number')
+            typeof block.timestamp ==='number' && 
+            typeof block.nonce === 'number')
     }
     constructor(
         index : number,
@@ -28,58 +30,55 @@ class Block {
         this.previousHash = previousHash;
         this.data = data;
         this.timestamp = timestamp;
+        this.nonce = 0;
     }
 }
-const genesisBlock:Block = new Block(0, "0101010", "", "Hello world", 123456);
 
-//blockchain has type of array of blocks. Typescript allows that only "Block" will be added to blockchain
-let blockChain : Block[] = [genesisBlock];
 
-//this would not able to added
-// blockChain.push("string");
+class Blockchain {
+    public chain : Block[]
+    constructor() {
+        this.chain = [this.createGenesis()]
+    }
+    //blockchain has type of array of blocks. Typescript allows that only "Block" will be added to blockchain
+    createGenesis = () : Block =>(new Block(0, "0101010", "", "Genesis Block", 123456))
+    
+    getLastestBlock = () : Block => this.chain[this.chain.length -1]; 
 
-const getBlockchain = () : Block[] => blockChain
+    getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000)
 
-const getLastestBlock = () : Block => blockChain[blockChain.length -1]; 
-
-const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000)
-
-const createNewBlock = (data : string) : Block=> {
-        const previousBlock : Block = getLastestBlock()
+    createNewBlock = (data : string) : Block=> {
+        const previousBlock : Block = this.getLastestBlock()
         const newIndex : number = previousBlock.index + 1
-        const nextTimeStamp : number = getNewTimeStamp()
+        const nextTimeStamp : number = this.getNewTimeStamp()
         const nextHash : string = Block.calculateBlockHash(newIndex, previousBlock.hash, nextTimeStamp, data)
         const newBlock : Block = new Block(newIndex, nextHash, previousBlock.hash, data, nextTimeStamp)
-        addBlock(newBlock);
+        this.addBlock(newBlock);
         return newBlock
-} 
-
-const getHashforABlock = (block : Block) : string => (Block.calculateBlockHash(block.index, block.previousHash, block.timestamp, block.data))
-const isValidBlock = (candidateBlock:Block , previousBlock: Block) : boolean => {
-    if(!Block.checkValidBlockStructure(candidateBlock)) {
-        return false;
-    } else if(previousBlock.index + 1 !== candidateBlock.index) {
-        return false;
-    } else if(previousBlock.hash !== candidateBlock.previousHash) {
-        return false;
-    } else if(getHashforABlock(candidateBlock) !== candidateBlock.hash) {
-        return false;
-    } else {
-        return true;
+    } 
+    
+    getHashforABlock = (block : Block) : string => (Block.calculateBlockHash(block.index, block.previousHash, block.timestamp, block.data))
+    
+    isValidBlock = (candidateBlock:Block , previousBlock: Block) : boolean => {
+        if(!Block.checkValidBlockStructure(candidateBlock)) {
+            return false;
+        } else if(previousBlock.index + 1 !== candidateBlock.index) {
+            return false;
+        } else if(previousBlock.hash !== candidateBlock.previousHash) {
+            return false;
+        } else if(this.getHashforABlock(candidateBlock) !== candidateBlock.hash) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    addBlock = (candidateBlock : Block) : void=> {
+        if(this.isValidBlock(candidateBlock, this.getLastestBlock())){
+            this.chain.push(candidateBlock)
+        }
     }
 }
-
-const addBlock = (candidateBlock : Block) : void=> {
-    if(isValidBlock(candidateBlock, getLastestBlock())){
-        blockChain.push(candidateBlock)
-    }
-}
-
-
-console.log(createNewBlock("second block"))
-console.log(createNewBlock("third block"))
-console.log(createNewBlock("fourth block"))
-console.log(createNewBlock("fifth block"))
 
 
 export {}
