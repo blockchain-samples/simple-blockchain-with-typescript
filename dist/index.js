ec2dfc55ee69eb7ff9c946501cd7eb8318d7e391
juntae <jkim3389@gatech.edu>
2020-04-24 21:55:19 -0400

    done typescript tutorial with simple blockchain implementation

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CrpytoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,8 +10,56 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CrpytoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.checkValidBlockStructure = (block) => {
+    return (typeof block.index === 'number' &&
+        typeof block.hash === 'string' &&
+        typeof block.previousHash === 'string' &&
+        typeof block.data === 'string' &&
+        typeof block.timestamp === 'number');
+};
 const genesisBlock = new Block(0, "0101010", "", "Hello world", 123456);
-//blockchain has type of array of blocks
+//blockchain has type of array of blocks. Typescript allows that only "Block" will be added to blockchain
 let blockChain = [genesisBlock];
-console.log(blockChain);
+//this would not able to added
+// blockChain.push("string");
+const getBlockchain = () => blockChain;
+const getLastestBlock = () => blockChain[blockChain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLastestBlock();
+    const newIndex = previousBlock.index + 1;
+    const nextTimeStamp = getNewTimeStamp();
+    const nextHash = Block.calculateBlockHash(newIndex, previousBlock.hash, nextTimeStamp, data);
+    const newBlock = new Block(newIndex, nextHash, previousBlock.hash, data, nextTimeStamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+const getHashforABlock = (block) => (Block.calculateBlockHash(block.index, block.previousHash, block.timestamp, block.data));
+const isValidBlock = (candidateBlock, previousBlock) => {
+    if (!Block.checkValidBlockStructure(candidateBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+    else if (getHashforABlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock) => {
+    if (isValidBlock(candidateBlock, getLastestBlock())) {
+        blockChain.push(candidateBlock);
+    }
+};
+console.log(createNewBlock("second block"));
+console.log(createNewBlock("third block"));
+console.log(createNewBlock("fourth block"));
+console.log(createNewBlock("fifth block"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import * as CrpytoJS from "crypto-js"
+
 class Block {
     public index : number;
     public hash : string;
     public previousHash : string;
     public data : string;
     public timestamp : number;
+
+    static calculateBlockHash = (index:number, previousHash:string, timestamp: number, data:string):string  => CrpytoJS.SHA256(index+previousHash+timestamp+data).toString();
+    static checkValidBlockStructure = (block : Block) : boolean => {
+        return (
+            typeof block.index === 'number' && 
+            typeof block.hash ==='string' && 
+            typeof block.previousHash ==='string' && 
+            typeof block.data ==='string' && 
+            typeof block.timestamp ==='number')
+    }
     constructor(
         index : number,
         hash : string,
@@ -20,9 +32,54 @@ class Block {
 }
 const genesisBlock:Block = new Block(0, "0101010", "", "Hello world", 123456);
 
-//blockchain has type of array of blocks
-let blockChain : [Block] = [genesisBlock];
+//blockchain has type of array of blocks. Typescript allows that only "Block" will be added to blockchain
+let blockChain : Block[] = [genesisBlock];
 
-console.log(blockChain)
+//this would not able to added
+// blockChain.push("string");
+
+const getBlockchain = () : Block[] => blockChain
+
+const getLastestBlock = () : Block => blockChain[blockChain.length -1]; 
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000)
+
+const createNewBlock = (data : string) : Block=> {
+        const previousBlock : Block = getLastestBlock()
+        const newIndex : number = previousBlock.index + 1
+        const nextTimeStamp : number = getNewTimeStamp()
+        const nextHash : string = Block.calculateBlockHash(newIndex, previousBlock.hash, nextTimeStamp, data)
+        const newBlock : Block = new Block(newIndex, nextHash, previousBlock.hash, data, nextTimeStamp)
+        addBlock(newBlock);
+        return newBlock
+} 
+
+const getHashforABlock = (block : Block) : string => (Block.calculateBlockHash(block.index, block.previousHash, block.timestamp, block.data))
+const isValidBlock = (candidateBlock:Block , previousBlock: Block) : boolean => {
+    if(!Block.checkValidBlockStructure(candidateBlock)) {
+        return false;
+    } else if(previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if(previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    } else if(getHashforABlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+const addBlock = (candidateBlock : Block) : void=> {
+    if(isValidBlock(candidateBlock, getLastestBlock())){
+        blockChain.push(candidateBlock)
+    }
+}
+
+
+console.log(createNewBlock("second block"))
+console.log(createNewBlock("third block"))
+console.log(createNewBlock("fourth block"))
+console.log(createNewBlock("fifth block"))
+
 
 export {}
